75f4d8593977ab24f4c75fa07177eb92b5d93534
Erik Schwarz <erik.schwarz@psi.ch>
2025-06-24 16:52:23 +0200

    added generated walls

--- pacman_ui/extract_walls.py
+++ pacman_ui/extract_walls.py
@@ -1,23 +1,23 @@
 import sys
 import xml.etree.ElementTree as ET
 
-def main()
+def main():
     tree = ET.parse(sys.argv[1])
     root = tree.getroot()
     entries = []
-    for button in root.findall(.widget[@class='QPushButton'])
-        rect = button.find(property[@name='geometry']rect)
-        if rect is None
+    for button in root.findall(".//widget[@class='QPushButton']"):
+        rect = button.find("./property[@name='geometry']/rect")
+        if rect is None:
             continue
-        x = rect.findtext(x)
-        y = rect.findtext(y)
-        width = rect.findtext(width)
-        height = rect.findtext(height)
+        x = rect.findtext("x")
+        y = rect.findtext("y")
+        width = rect.findtext("width")
+        height = rect.findtext("height")
         entries.append((x, y, width, height))
-    print({)
-    for x, y, w, h in entries
-        print(f    {{{x}, {y}, {w}, {h}}},)
-    print(})
+    print("{")
+    for x, y, w, h in entries:
+        print(f"    {{{x}, {y}, {w}, {h}}},")
+    print("}")
 
-if __name__ == __main__
+if __name__ == "__main__":
     main()
