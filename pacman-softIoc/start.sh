a76eee11204a5356af0614c17fd76cca3bc81668
CraftingDragon007 <craftingdragon007@outlook.com>
2025-08-12 16:03:40 +0200

    Made pacman running again, still needs some work in terms of user input control (don't switch directions if not perfectly aligned, but also allow the user to turn around by just pressing the button once and not on the exact timing)

--- pacman-softIoc/start.sh
+++ pacman-softIoc/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
-# This script is used to start the pong epics soft ioc
+# This script is used to start the pacman epics soft ioc
 export EPICS_DB_INCLUDE_PATH=$EPICS_BASE/dbd:$(pwd)
-export PONG_PATH=$(pwd)
-export EPICS_DRIVER_PATH=$PONG_PATH/O.$EPICS_HOST_ARCH:$EPICS_BASE/lib/$EPICS_HOST_ARCH
+export PACMAN_PATH=$(pwd)
+export EPICS_DRIVER_PATH=$PACMAN_PATH/O.$EPICS_HOST_ARCH:$EPICS_BASE/lib/$EPICS_HOST_ARCH
 export EPICS_PATH=$EPICS_BASE
 export LD_LIBRARY_PATH=$EPICS_BASE/lib/$EPICS_HOST_ARCH
 export EPICS_CA_SERVER_PORT=5064
 
-softIoc -D $EPICS_BASE/dbd/softIoc.dbd -v $PONG_PATH/startup.script
+softIoc -D $EPICS_BASE/dbd/softIoc.dbd -v $PACMAN_PATH/startup.script
