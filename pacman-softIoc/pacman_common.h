c26ab1c24435e0d0113e6aa64fc899db15e04232
CraftingDragon007 <craftingdragon007@outlook.com>
2025-08-18 16:39:36 +0200

    Added game engine logic, added frightened mode when eating large food, counting score for eating costs, added pacman death (with animation), moved duplicated defines into a single shared header file

---
+++ pacman-softIoc/pacman_common.h
@@ -0,0 +1,38 @@
+#define MODE_WAIT 0
+#define MODE_SCATTER 1
+#define MODE_CHASE 2
+#define MODE_FRIGHT 3
+#define MODE_SPAWN 4
+
+#define DIRECTION_RIGHT 0
+#define DIRECTION_LEFT 1
+#define DIRECTION_UP 2
+#define DIRECTION_DOWN 3
+
+#define bool int
+#define true 1
+#define false 0
+
+#define PICK_MAX 1
+#define PICK_MIN 0
+
+#define ANGLE_INVALID -1.0
+#define ANGLE_MIN 0.0
+#define ANGLE_MAX 3.15
+
+#define FIELD_WIDTH 28
+#define FIELD_HEIGHT 31
+#define TILE_SIZE 32
+
+#define TILE_PATH 0
+#define TILE_WALL 1
+#define TILE_PORTAL 2
+#define TILE_SPAWN_ENTRY 3
+
+#define INVALID_DIRECTION -1
+#define READY_DIRECTION -1
+
+#define STATE_NORMAL 0
+#define STATE_BLOCKED 1
+#define STATE_TELEPORT 2
+#define STATE_READY 3
